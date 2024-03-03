3e0d51214cc14c9545d90c549f192836575d119b
Allayar Jandullaev <jandullaevallayar@gmail.com>
2024-03-03 23:48:37 +0500

    Replaced

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -3,7 +3,7 @@
 
 //   ==========   FETCHING METHOD GET   ==========   //
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("../library/library.json")
+  fetch("assets/library/library.json")
     .then((response) => response.json())
     .then((data) => {
       displayBooks(data.books);
